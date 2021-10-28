0171bdc6d85433c7f356c222ae4bacb6d4d7d080
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-10-28 11:42:42 -0500

    [Add] Faker and GET: query params

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const faker = require('faker')
+
 const app = express();
 const port = 3000;
 
@@ -10,10 +12,48 @@ app.get('/nueva-ruta', (req, res)=> {
   res.send('Hola, soy un nuevo endpoint');
 });
 
-app.get('/productos', (req, res)=> {
+app.get('/products', (req, res)=> {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+
+  }
+  res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
   res.json({
-    name: 'Producto 1',
-    price: 1000
+    id,
+    name: 'Producto 2',
+    price: 800
+  });
+});
+
+app.get('/users', (req,res) => {
+  const { limit, offset } = req.query;
+  if(limit && offset) {
+    res.json({
+      limit,
+      offset
+    })
+  } else{
+    res.send('no hay parámetros');
+  }
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId
   });
 });
 
